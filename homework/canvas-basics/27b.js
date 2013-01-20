b90990fd2c3f25b1fdc27f43e09211629eddc1bb
Haley Young <haleyyoung88@gmail.com>
2013-01-20 08:11:52 -0800

    Figured out how to color the sides differently

--- homework/canvas-basics/27b.js
+++ homework/canvas-basics/27b.js
@@ -10,36 +10,48 @@
     var canvas = document.getElementById("canvas"),
         renderingContext = canvas.getContext("2d");
 
-    //Creating two overlapping squares
-    renderingContext.fillStyle = "white";
-    renderingContext.beginPath();
+    renderingContext.globalAlpha = 0.5;
     //Back Side
+    renderingContext.fillStyle = "blue";
+    renderingContext.beginPath();
     renderingContext.moveTo(361, 361);
     renderingContext.lineTo(361, 461);
     renderingContext.lineTo(461, 461);
     renderingContext.lineTo(461, 361);
     renderingContext.lineTo(361, 361);
+    renderingContext.fill();
     //Front side
+    renderingContext.fillStyle = "green";
+    renderingContext.beginPath();
     renderingContext.moveTo(511, 511);
     renderingContext.lineTo(511, 411);
     renderingContext.lineTo(411, 411);
     renderingContext.lineTo(411, 511);
     renderingContext.lineTo(511, 511);
+    renderingContext.fill();
+    //Right Line to connect
+    renderingContext.fillStyle = "red";
+    renderingContext.beginPath();
+    renderingContext.moveTo(511,511);
+    renderingContext.lineTo(461,461);
+    renderingContext.stroke();
     //Top Side
+    renderingContext.fillStyle = "yellow";
+    renderingContext.beginPath();
     renderingContext.moveTo(361,361);
     renderingContext.lineTo(411,411);
     renderingContext.lineTo(511,411);
     renderingContext.lineTo(461,361);
     renderingContext.lineTo(361,361);
+    renderingContext.fill();
     //Left Side
+    renderingContext.fillStyle = "purple";
+    renderingContext.beginPath();
     renderingContext.moveTo(361,461);
     renderingContext.lineTo(411,511);
     renderingContext.lineTo(411,411);
     renderingContext.lineTo(361,361);
     renderingContext.lineTo(361,461);
-    //Right Line to finish
-    renderingContext.moveTo(511,511);
-    renderingContext.lineTo(461,461);
     renderingContext.fill();
 /*    renderingContext.moveTo(362,462);
     renderingContext.lineTo(412,512);
