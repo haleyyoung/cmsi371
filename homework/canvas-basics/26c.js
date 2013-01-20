4316f44e7450e1064c8047623dea2524a5e54dbd
Haley Young <haleyyoung88@gmail.com>
2013-01-19 20:41:02 -0800

    Cleaned up the files

--- homework/canvas-basics/26c.js
+++ homework/canvas-basics/26c.js
@@ -1,8 +1,8 @@
 /*
- * This javaScript file is a canvas-based web page which generates two overlapping
- * rectangles which are 50% transparent. One red and one blue rectangle creating
- * purple in the overlapped area, per request of problem 25c. Note the colors 
- * have been changed from the ones requested.
+ * This javaScript file is a canvas-based web page which generates squares 1
+ * pixel apart filling the entire canvas. The squares go from light blue to
+ * feucia, per request of problem 26c. Note the colors have been changed from
+ * the ones requested.
  */
 
 (function () {
@@ -10,7 +10,7 @@
     var canvas = document.getElementById("canvas"),
         renderingContext = canvas.getContext("2d");
 
-    //Creating the rectangles, globalAlpha deals with the transparency
+    //Creating the squares as well as filling them in
     for(var i = 0; i < 253; i+=5){
         for(var j = 0; j < 253; j+=5){
             renderingContext.fillStyle = 'rgb('+(51+j)+','+(150-j)+', 256)';
