aa29032529e001a0c1422ef3a6e55c7ca23106e0
Haley Young <haleyyoung88@gmail.com>
2013-01-20 08:38:58 -0800

    Reread problem and realized it was in the wrong place on the canvas

--- homework/canvas-basics/27b.js
+++ homework/canvas-basics/27b.js
@@ -11,47 +11,48 @@
         renderingContext = canvas.getContext("2d");
     //Make the outline blue
     renderingContext.strokeStyle = "blue";
+    renderingContext.globalAlpha = 0.5;
     //Back Side
     renderingContext.beginPath();
-    renderingContext.moveTo(361, 361);
-    renderingContext.lineTo(361, 461);
-    renderingContext.lineTo(461, 461);
-    renderingContext.lineTo(461, 361);
-    renderingContext.lineTo(361, 361);
+    renderingContext.moveTo(180, 0);
+    renderingContext.lineTo(180, 99);
+    renderingContext.lineTo(280, 99);
+    renderingContext.lineTo(280, 0);
+    renderingContext.lineTo(180, 0);
     renderingContext.stroke();
     //Right Line to connect
     renderingContext.beginPath();
-    renderingContext.moveTo(511,511);
-    renderingContext.lineTo(461,461);
+    renderingContext.moveTo(280,99);
+    renderingContext.lineTo(330,149);
     renderingContext.stroke();
     //Front side
     renderingContext.fillStyle = "#008FB2";
     renderingContext.beginPath();
-    renderingContext.moveTo(511, 511);
-    renderingContext.lineTo(511, 411);
-    renderingContext.lineTo(411, 411);
-    renderingContext.lineTo(411, 511);
-    renderingContext.lineTo(511, 511);
+    renderingContext.moveTo(330, 149);
+    renderingContext.lineTo(330, 49);
+    renderingContext.lineTo(230, 49);
+    renderingContext.lineTo(230, 149);
+    renderingContext.lineTo(330, 149);
     renderingContext.fill();
     renderingContext.stroke();
     //Top Side
     renderingContext.fillStyle = "#00CCFF";
     renderingContext.beginPath();
-    renderingContext.moveTo(361,361);
-    renderingContext.lineTo(411,411);
-    renderingContext.lineTo(511,411);
-    renderingContext.lineTo(461,361);
-    renderingContext.lineTo(361,361);
+    renderingContext.moveTo(180,0);
+    renderingContext.lineTo(230,49);
+    renderingContext.lineTo(330,49);
+    renderingContext.lineTo(280,0);
+    renderingContext.lineTo(180,0);
     renderingContext.fill();
     renderingContext.stroke();
     //Left Side
     renderingContext.fillStyle = "#005266";
     renderingContext.beginPath();
-    renderingContext.moveTo(361,461);
-    renderingContext.lineTo(411,511);
-    renderingContext.lineTo(411,411);
-    renderingContext.lineTo(361,361);
-    renderingContext.lineTo(361,461);
+    renderingContext.moveTo(180,99);
+    renderingContext.lineTo(230,149);
+    renderingContext.lineTo(230,49);
+    renderingContext.lineTo(180,0);
+    renderingContext.lineTo(180,99);
     renderingContext.fill();
     renderingContext.stroke();
 }());
