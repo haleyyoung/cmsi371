7282087696ff0aa5ce1a45a22207371d4b6256eb
Haley Young <haleyyoung88@gmail.com>
2013-01-19 21:31:49 -0800

    Got an offset, but it's a little off

--- homework/canvas-basics/26c.js
+++ homework/canvas-basics/26c.js
@@ -12,16 +12,20 @@
     j = 0;
     //Generating multiple hexagons in an offset grid
     for(var i = 0; i < 256; i += 45){
-        for(var j = 0; j < 256; j += 45){
+        var j = 0;
+        if(i%90 === 0){
+            j = 19;
+        }
+        for(j; j < 256; j += 45){
             //Creating the hexagon
             renderingContext.fillStyle = "#CC00FF";
             renderingContext.beginPath();
-            renderingContext.moveTo(i,j + 20);
-            renderingContext.lineTo(i + 10, j);
-            renderingContext.lineTo(i + 30, j);
-            renderingContext.lineTo(i + 40, j + 20);
-            renderingContext.lineTo(i + 30, j + 40);
-            renderingContext.lineTo(i + 10, j + 40);
+            renderingContext.moveTo(j,i + 20);
+            renderingContext.lineTo(j + 10, i);
+            renderingContext.lineTo(j + 30, i);
+            renderingContext.lineTo(j + 40, i + 20);
+            renderingContext.lineTo(j + 30, i + 40);
+            renderingContext.lineTo(j + 10, i + 40);
             renderingContext.fill();
         }
     }
