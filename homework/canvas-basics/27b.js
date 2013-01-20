d003098246bcf243a6c54bd8b3da3797b5e0bf6f
Haley Young <haleyyoung88@gmail.com>
2013-01-20 08:19:27 -0800

    Put outlines on the whole thing

--- homework/canvas-basics/27b.js
+++ homework/canvas-basics/27b.js
@@ -9,21 +9,23 @@
     // Using jQuery here.
     var canvas = document.getElementById("canvas"),
         renderingContext = canvas.getContext("2d");
-
-    renderingContext.globalAlpha = 0.5;
-    renderingContext.strokeStyle = "red";
+    //Make the outline blue
+    renderingContext.strokeStyle = "blue";
     //Back Side
-    renderingContext.fillStyle = "blue";
     renderingContext.beginPath();
     renderingContext.moveTo(361, 361);
     renderingContext.lineTo(361, 461);
     renderingContext.lineTo(461, 461);
     renderingContext.lineTo(461, 361);
     renderingContext.lineTo(361, 361);
-    renderingContext.fill();
+    renderingContext.stroke();
+    //Right Line to connect
+    renderingContext.beginPath();
+    renderingContext.moveTo(511,511);
+    renderingContext.lineTo(461,461);
     renderingContext.stroke();
     //Front side
-    renderingContext.fillStyle = "green";
+    renderingContext.fillStyle = "#008FB2";
     renderingContext.beginPath();
     renderingContext.moveTo(511, 511);
     renderingContext.lineTo(511, 411);
@@ -32,13 +34,8 @@
     renderingContext.lineTo(511, 511);
     renderingContext.fill();
     renderingContext.stroke();
-    //Right Line to connect
-    renderingContext.beginPath();
-    renderingContext.moveTo(511,511);
-    renderingContext.lineTo(461,461);
-    renderingContext.stroke();
     //Top Side
-    renderingContext.fillStyle = "yellow";
+    renderingContext.fillStyle = "#00CCFF";
     renderingContext.beginPath();
     renderingContext.moveTo(361,361);
     renderingContext.lineTo(411,411);
@@ -48,7 +45,7 @@
     renderingContext.fill();
     renderingContext.stroke();
     //Left Side
-    renderingContext.fillStyle = "purple";
+    renderingContext.fillStyle = "#005266";
     renderingContext.beginPath();
     renderingContext.moveTo(361,461);
     renderingContext.lineTo(411,511);
@@ -56,9 +53,5 @@
     renderingContext.lineTo(361,361);
     renderingContext.lineTo(361,461);
     renderingContext.fill();
-/*    renderingContext.moveTo(362,462);
-    renderingContext.lineTo(412,512);
-    renderingContext.moveTo(462,362);
-    renderingContext.lineTo(512,412);*/
     renderingContext.stroke();
 }());
