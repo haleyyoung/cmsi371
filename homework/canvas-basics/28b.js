f4cc1787dfa38f500665cbf3be50f41ed50bb094
Haley Young <haleyyoung88@gmail.com>
2013-01-21 18:53:22 -0800

    Added a gradient to the water

--- homework/canvas-basics/28b.js
+++ homework/canvas-basics/28b.js
@@ -8,21 +8,28 @@
     // Using jQuery here.
     var canvas = document.getElementById("canvas"),
         renderingContext = canvas.getContext("2d"),
-        linearGradient = renderingContext.createLinearGradient(0,0,0,250);
+        linearGradientSky = renderingContext.createLinearGradient(0,0,0,250),
+        linearGradientWater = renderingContext.createLinearGradient(0,250,0,512);
 
     //Colors for the sky
-    linearGradient.addColorStop(0, "#FFFF66");
-    linearGradient.addColorStop(0.3, "#FF4719");
-    linearGradient.addColorStop(0.6, "#FF1975");
-    linearGradient.addColorStop(1, "#660066");
+    linearGradientSky.addColorStop(0, "#FFFF66");
+    linearGradientSky.addColorStop(0.3, "#FF4719");
+    linearGradientSky.addColorStop(0.6, "#FF1975");
+    linearGradientSky.addColorStop(1, "#660066");
 
     //Sky
-    renderingContext.fillStyle = linearGradient;
+    renderingContext.fillStyle = linearGradientSky;
     renderingContext.fillRect(0,0,512,512);
     renderingContext.fill();
-    
+
+    //Colors for the water
+    linearGradientWater.addColorStop(0, "#008F00");
+    linearGradientWater.addColorStop(0.05, "#0066CC");
+    linearGradientWater.addColorStop(0.4, "#003366");
+    linearGradientWater.addColorStop(1, "#000F1F");
+
     //Water
-    renderingContext.fillStyle = "blue";
+    renderingContext.fillStyle = linearGradientWater;
     renderingContext.fillRect(0,250,512,261);
     renderingContext.fill();
     
