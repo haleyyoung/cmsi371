cc0655c2af08fbfa66dfd2cb228db2429f24822b
Haley Young <haleyyoung88@gmail.com>
2013-01-26 16:50:04 -0800

    Pulled out the data from the objects

--- homework/canvas-basics/25e.js
+++ homework/canvas-basics/25e.js
@@ -8,15 +8,17 @@
     // Using jQuery here.
     var canvas = document.getElementById("canvas"),
         renderingContext = canvas.getContext("2d");
+    
+    var hexagonVertices = [[56,256],[156,56],[356,56],[456,256],[356,456],[156,456]];
 
     //Creating the hexagon
     renderingContext.fillStyle = "#CC00FF";
     renderingContext.beginPath();
-    renderingContext.moveTo(56,256);
-    renderingContext.lineTo(156,56);
-    renderingContext.lineTo(356,56);
-    renderingContext.lineTo(456,256);
-    renderingContext.lineTo(356,456);
-    renderingContext.lineTo(156,456);
+    renderingContext.moveTo(hexagonVertices[0][0], hexagonVertices[0][1]);
+    renderingContext.lineTo(hexagonVertices[1][0], hexagonVertices[1][1]);
+    renderingContext.lineTo(hexagonVertices[2][0], hexagonVertices[2][1]);
+    renderingContext.lineTo(hexagonVertices[3][0], hexagonVertices[3][1]);
+    renderingContext.lineTo(hexagonVertices[4][0], hexagonVertices[4][1]);
+    renderingContext.lineTo(hexagonVertices[5][0], hexagonVertices[5][1]);
     renderingContext.fill();
 }());
