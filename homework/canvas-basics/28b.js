30e4e6743ae81822b111caca8845776d7a70d5ca
Haley Young <haleyyoung88@gmail.com>
2013-01-27 19:02:29 -0800

    Cleaned up spacing as well as abstracted background elements

--- homework/canvas-basics/28b.js
+++ homework/canvas-basics/28b.js
@@ -11,7 +11,23 @@
         linearGradientSky = renderingContext.createLinearGradient(0,0,0,250),
         linearGradientWater = renderingContext.createLinearGradient(0,250,0,512),
         radialGradientSun = renderingContext.createRadialGradient(250, 250, 1, 180, 180, 320);
-        
+
+    // Colors for the sky
+    linearGradientSky.addColorStop(0, "#FFFF66");
+    linearGradientSky.addColorStop(0.3, "#FF4719");
+    linearGradientSky.addColorStop(0.6, "#FF1975");
+    linearGradientSky.addColorStop(1, "#660066");
+
+    // Colors for the water
+    linearGradientWater.addColorStop(0, "#008F00");
+    linearGradientWater.addColorStop(0.05, "#0066CC");
+    linearGradientWater.addColorStop(0.4, "#003366");
+    linearGradientWater.addColorStop(1, "#000F1F");
+
+    // Colors for the sun
+    radialGradientSun.addColorStop(0, "#FF9900");
+    radialGradientSun.addColorStop(0.5, "red");
+
     var sceneElements = { sky: { vertices: [[0,0],[512,512]],
                                  color: linearGradientSky
                                },
@@ -30,42 +46,16 @@
                                         color: radialGradientSun
                                       }
                         };
-                        
 
-    // Colors for the sky
-    linearGradientSky.addColorStop(0, "#FFFF66");
-    linearGradientSky.addColorStop(0.3, "#FF4719");
-    linearGradientSky.addColorStop(0.6, "#FF1975");
-    linearGradientSky.addColorStop(1, "#660066");
+    // This is a function that draws a rectangular shape with a gradient
+    var drawBackground = function(part){
+        renderingContext.fillStyle = part.color;
+        renderingContext.fillRect(part.vertices[0][0], part.vertices[0][1],
+                                  part.vertices[1][0], part.vertices[1][1]);
+        renderingContext.fill();
+    }
 
-    // Colors for the water
-    linearGradientWater.addColorStop(0, "#008F00");
-    linearGradientWater.addColorStop(0.05, "#0066CC");
-    linearGradientWater.addColorStop(0.4, "#003366");
-    linearGradientWater.addColorStop(1, "#000F1F");
-
-    // Sky
-    renderingContext.fillStyle = linearGradientSky;
-    renderingContext.fillRect(0,0,512,512);
-    renderingContext.fill();
-
-
-    // Water
-    renderingContext.fillStyle = linearGradientWater;
-    renderingContext.fillRect(0,250,512,261);
-    renderingContext.fill();
-
-    // Colors for the sun
-    radialGradientSun.addColorStop(0, "#FF9900");
-    radialGradientSun.addColorStop(0.5, "red");
-
-    // Sun
-/*    renderingContext.fillStyle = radialGradientSun;
-    renderingContext.beginPath();
-    renderingContext.arc(250,250,100,0,Math.PI,true);
-    renderingContext.fill();*/
-
-    // Sun Reflection
+    // This is a funtion that draws a circle with a gradient
     var drawSun = function(part){
         renderingContext.fillStyle = part.color;
         if(part.opacity){
@@ -77,8 +67,10 @@
                              part.endpoints[0], part.endpoints[1],true);
         renderingContext.fill();
     }
-    
+
+    drawBackground(sceneElements.sky);
+    drawBackground(sceneElements.water);
     drawSun(sceneElements.sun);
     drawSun(sceneElements.reflection);
-    
+
 }());
