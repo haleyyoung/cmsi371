0539528d52963a7e2d0f1d2822b59d2da9212794
Haley Young <haleyyoung88@gmail.com>
2013-01-27 17:04:56 -0800

    Got the cube completely separated from its data

--- homework/canvas-basics/27b.js
+++ homework/canvas-basics/27b.js
@@ -1,6 +1,6 @@
 /*
  * This javaScript file is a canvas-based web page which generates a 3d cube wire
- * frame with sides of different shades, per request of problem 27b. Note the colors 
+ * frame with sides of different shades, per request of problem 27b. Note the colors
  * have been changed from the ones requested.
  */
 
@@ -8,16 +8,22 @@
     // Using jQuery here.
     var canvas = document.getElementById("canvas"),
         renderingContext = canvas.getContext("2d");
-        
-    var backSquare = {vertices: [[180,0],[180,99],[280,99],[280,0]]
-                     },
-        frontSquare = {vertices: [[330,149],[330,49],[230,49],[230,149]],
-                       color: "#008FB2"
-                      };
 
-    //Make the outline blue
-    renderingContext.strokeStyle = "blue";
-    //Back Side
+    var backSquare = { vertices: [[180,0],[180,99],[280,99],[280,0]]
+                     },
+        frontSquare = { vertices: [[330,149],[330,49],[230,49],[230,149]],
+                        color: "#008FB2"
+                      },
+
+        topSquare = { vertices: [[180,0],[230,49],[330,49],[280,0]],
+                       color: "#00CCFF"
+                     },
+        leftSquare = { vertices: [[180,99],[230,149],[230,49],[180,0]],
+                       color: "#005266"
+                     };
+
+
+    // This is a function that takes in a side-object and draws it
     var drawSide = function (sideName){
         renderingContext.beginPath();
         renderingContext.moveTo(sideName.vertices[0][0], sideName.vertices[0][1]);
@@ -31,43 +37,20 @@
             renderingContext.fill();
         }
     }
-    
+
+    // Make the outline blue
+    renderingContext.strokeStyle = "blue";
+
     drawSide(backSquare);
+
     //Right Line to connect
     renderingContext.beginPath();
     renderingContext.moveTo(280,99);
     renderingContext.lineTo(330,149);
     renderingContext.stroke();
-    //Front side
-    
+
     drawSide(frontSquare);
-   /* 
-    renderingContext.beginPath();
-    renderingContext.moveTo(330, 149);
-    renderingContext.lineTo(330, 49);
-    renderingContext.lineTo(230, 49);
-    renderingContext.lineTo(230, 149);
-    renderingContext.lineTo(330, 149);
-    renderingContext.fill();
-    renderingContext.stroke();*/
-    //Top Side
-    renderingContext.fillStyle = "#00CCFF";
-    renderingContext.beginPath();
-    renderingContext.moveTo(180,0);
-    renderingContext.lineTo(230,49);
-    renderingContext.lineTo(330,49);
-    renderingContext.lineTo(280,0);
-    renderingContext.lineTo(180,0);
-    renderingContext.fill();
-    renderingContext.stroke();
-    //Left Side
-    renderingContext.fillStyle = "#005266";
-    renderingContext.beginPath();
-    renderingContext.moveTo(180,99);
-    renderingContext.lineTo(230,149);
-    renderingContext.lineTo(230,49);
-    renderingContext.lineTo(180,0);
-    renderingContext.lineTo(180,99);
-    renderingContext.fill();
-    renderingContext.stroke();
+    drawSide(topSquare);
+    drawSide(leftSquare);
+
 }());
