f7b4a7264e0876f2a54bb21acaf8b14509910bf5
Haley Young <haleyyoung88@gmail.com>
2013-02-02 17:31:37 -0800

    Got the sprites to move properly for the beginning of the scene

--- homework/animation-sprite-2.0/keyframe-tweening-sprites.js
+++ homework/animation-sprite-2.0/keyframe-tweening-sprites.js
@@ -14,15 +14,15 @@
                             colorStop3: 0.7
         },
 
-        minionVertices = { start: {x: 400, y: 350},
-                           cp1: {x: 250, y: 350},
-                           cp2: {x: 250, y: 650},
-                           bottom: {x: 400, y: 650},
-                           cp3: {x: 550, y: 650},
-                           cp4: {x: 550, y: 350},
+        minionVertices = { start: {x: 200, y: 300},
+                           cp1: {x: 50, y: 300},
+                           cp2: {x: 50, y: 600},
+                           bottom: {x: 200, y: 600},
+                           cp3: {x: 350, y: 600},
+                           cp4: {x: 350, y: 300},
                            color: "yellow"
                          },
-        eyes = { ball: {x:400, y:450},
+        eyes = { ball: {x:200, y: 400},
                  ballRadius: 30,
                  ballColor: "white",
                  pupilRadius:10,
@@ -32,45 +32,51 @@
                  goggleRadius: 30,
                  goggleColor: "gray" 
         },
-        mouth = { left: {x: 380, y: 510},
-                  cp1: {x: 400, y: 530},
-                  cp2: {x: 420, y: 540},
-                  right: {x: 430, y: 510},
-                  cp3: {x: 420, y: 490},
-                  cp4: {x: 400, y: 490},
+        mouth = { left: {x: 180, y: 460},
+                  cp1: {x: 200, y: 480},
+                  cp2: {x: 220, y: 490},
+                  right: {x: 230, y: 460},
+                  cp3: {x: 220, y: 440},
+                  cp4: {x: 200, y: 440},
                   color: "black"
         },
-        legs = { corner: {x: 350, y:630},
+        legs = { corner: {x: 150, y:580},
                  width: 40,
                  height: 40,
                  color: "blue"
         },
-        cupcakeVertices = { candle: { corner: {x: 75, y:50},
+        cupcakeVertices = { candle: { corner: {x: 75, y:520},
                                       width: 10,
                                       height: 30,
                                       color: "red"
                                      
                                     },
                             frosting: {
-                                       start: {x: 40, y:100},
-                                       cp1: {x: 50, y: 80},
-                                       cp2: {x: 60, y: 75},
-                                       cp3: {x: 70, y: 60},
-                                       cp4: {x: 80, y: 60},
-                                       cp5: {x: 80, y: 60},
-                                       cp6: {x: 90, y: 60},
-                                       cp7: {x: 100, y: 75},
-                                       cp8: {x: 110, y: 80},
-                                       end: {x: 120, y: 100},
+                                       start: {x: 40, y:570},
+                                       cp1: {x: 50, y: 550},
+                                       cp2: {x: 60, y: 545},
+                                       cp3: {x: 70, y: 530},
+                                       cp4: {x: 80, y: 530},
+                                       cp5: {x: 80, y: 530},
+                                       cp6: {x: 90, y: 530},
+                                       cp7: {x: 100, y: 545},
+                                       cp8: {x: 110, y: 550},
+                                       end: {x: 120, y: 570},
                                        color: "purple"
                                       },
                             cake: {
-                                   topLeft: {x: 40, y: 100},
-                                   bottomLeft: {x: 50, y: 150},
-                                   bottomRight: {x: 110, y: 150},
-                                   topRight: {x: 120, y: 100},
-                                   cp1: {x: 70, y: 120},
+                                   topLeft: {x: 40, y: 570},
+                                   bottomLeft: {x: 50, y: 620},
+                                   bottomRight: {x: 110, y: 620},
+                                   topRight: {x: 120, y: 570},
+                                   cp1: {x: 70, y: 590},
                                    color: "turquoise"
+                                  }
+                            fire: {
+                                   bottom: {x: 75, y: 520},
+                                   left: {x: 70, y:510},
+                                   top: {x: 75, y: 480},
+                                   right: {x: 80, y: 510}
                                   }
         },
 
@@ -192,13 +198,13 @@
                 keyframes: [
                     {
                         frame: 0,
-                        tx: 20,
-                        ty: 20,
-                        ease: KeyframeTweener.linear
+                        tx: 1000,
+                        ty: 0,
+                        ease: KeyframeTweener.quadEaseInOut
                     },
 
                     {
-                        frame: 30,
+                        frame: 100,
                         tx: 100,
                         ty: 50,
                         ease: KeyframeTweener.quadEaseInOut
@@ -218,13 +224,13 @@
                 keyframes: [
                     {
                         frame: 0,
-                        tx: 20,
-                        ty: 20,
-                        ease: KeyframeTweener.linear
+                        tx: 1000,
+                        ty: 0,
+                        ease: KeyframeTweener.quadEaseInOut
                     },
 
                     {
-                        frame: 30,
+                        frame: 100,
                         tx: 100,
                         ty: 50,
                         ease: KeyframeTweener.quadEaseInOut
@@ -244,13 +250,24 @@
                 keyframes: [
                     {
                         frame: 0,
+                        tx: 0,
+                        ty: 0
+                    },
+                    
+                    {
+                        frame: 500,
+                        tx: 0,
+                        ty: 0
+                    },
+                    {
+                        frame: 700,
                         tx: 20,
                         ty: 20,
                         ease: KeyframeTweener.linear
                     },
 
                     {
-                        frame: 30,
+                        frame: 1110,
                         tx: 100,
                         ty: 50,
                         ease: KeyframeTweener.quadEaseInOut
