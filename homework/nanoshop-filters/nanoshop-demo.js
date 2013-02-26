6f145676056e7741922e3a13f82045dc2cd1c110
Haley Young <haleyyoung88@gmail.com>
2013-02-26 15:34:37 -0800

    Added buttons to the nanoshop files as well

--- homework/nanoshop-filters/nanoshop-demo.js
+++ homework/nanoshop-filters/nanoshop-demo.js
@@ -6,28 +6,35 @@
     var canvas = $("#picture")[0],
         renderingContext = canvas.getContext("2d"),
         gradient;
-    alert("colorAccentuate");
-    renderingContext.putImageData(
-        Nanoshop.applyFilter(
-            renderingContext.getImageData(0, 0, canvas.width, canvas.height),
-            colorAccentuate, "blue"),
-        0,
-        0
-    );
-    alert("primaryColors");
-    renderingContext.putImageData(
-        Nanoshop.applyFilter(
-            renderingContext.getImageData(0, 0, canvas.width, canvas.height),
-            primaryColors),
-        0,
-        0
-    );
-    alert("basicDarkener");
-    renderingContext.putImageData(
-        Nanoshop.applyFilter(
-            renderingContext.getImageData(0, 0, canvas.width, canvas.height),
-            basicDarkener),
-        0,
-        0
-    );
+    $("#apply-filter-colorAccentuate").click(function () {
+        // This filter, you can pick "red", "green", or "blue" for the last
+        // parameter of renderingContext.getImageData
+        renderingContext.putImageData(
+            Nanoshop.applyFilter(
+                renderingContext.getImageData(0, 0, canvas.width, canvas.height),
+                colorAccentuate, "blue"),
+            0,
+            0
+        );
+    });
+
+    $("#apply-filter-primaryColors").click(function () {
+        renderingContext.putImageData(
+            Nanoshop.applyFilter(
+                renderingContext.getImageData(0, 0, canvas.width, canvas.height),
+                primaryColors),
+            0,
+            0
+        );
+    });
+
+    $("#apply-filter-darkener").click(function () {
+        renderingContext.putImageData(
+            Nanoshop.applyFilter(
+                renderingContext.getImageData(0, 0, canvas.width, canvas.height),
+                basicDarkener),
+            0,
+            0
+        );
+    });
 }());
