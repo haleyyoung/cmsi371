6d6f20b9b33bfbf312b7e6720464b913729c6bb1
Haley Young <haleyyoung88@gmail.com>
2013-03-06 13:59:32 -0800

    Working on a triangular prism with rectangular base. Like the top of a house

---
+++ homework/pipeline/shapes.js
@@ -0,0 +1,138 @@
+/*
+ * This module defines/generates vertex arrays for certain predefined shapes.
+ * The "shapes" are returned as indexed vertices, with utility functions for
+ * converting these into "raw" coordinate arrays.
+ */
+var Shapes = {
+    /*
+     * Returns the vertices for a small icosahedron.
+     */
+    icosahedron: function () {
+        // These variables are actually "constants" for icosahedron coordinates.
+        var X = 0.525731112119133606,
+            Z = 0.850650808352039932;
+
+        return {
+            vertices: [
+                [ -X, 0.0, Z ],
+                [ X, 0.0, Z ],
+                [ -X, 0.0, -Z ],
+                [ X, 0.0, -Z ],
+                [ 0.0, Z, X ],
+                [ 0.0, Z, -X ],
+                [ 0.0, -Z, X ],
+                [ 0.0, -Z, -X ],
+                [ Z, X, 0.0 ],
+                [ -Z, X, 0.0 ],
+                [ Z, -X, 0.0 ],
+                [ -Z, -X, 0.0 ]
+            ],
+
+            indices: [
+                [ 1, 4, 0 ],
+                [ 4, 9, 0 ],
+                [ 4, 5, 9 ],
+                [ 8, 5, 4 ],
+                [ 1, 8, 4 ],
+                [ 1, 10, 8 ],
+                [ 10, 3, 8 ],
+                [ 8, 3, 5 ],
+                [ 3, 2, 5 ],
+                [ 3, 7, 2 ],
+                [ 3, 10, 7 ],
+                [ 10, 6, 7 ],
+                [ 6, 11, 7 ],
+                [ 6, 0, 11 ],
+                [ 6, 1, 0 ],
+                [ 10, 1, 6 ],
+                [ 11, 0, 9 ],
+                [ 2, 11, 9 ],
+                [ 5, 2, 9 ],
+                [ 11, 2, 7 ]
+            ]
+        };
+    },
+    
+    /*
+     * Returns the vertices for an elongated sphere.
+     */
+    triangularPrismRectangleBase: function () {
+        // These variables are actually "constants" for trangular prism coordinates.
+        var X = 0.75,
+            Y = 0.5,
+            Z = -0.75;
+
+        return {
+            vertices: [
+                [ X, 0.0, Z ],
+                [ -X, 0.0, Z ],
+                [ 0.0, Y, Z ]
+                [ X, 0.0, -Z ],
+                [ -X, 0.0, -Z ],
+                [ 0.0, Y, -Z ]
+            ],
+
+            indices: [
+                [ 0, 1, 2 ],
+                [ 0, 2, 5, 3 ],  // Trapezoid
+                [ 3, 4, 5 ],
+                [ 4, 5, 2, 1 ],  // Trapezoid
+                [ 1, 8, 4 ],
+                [ 0, 1, 4, 3 ], // Trapezoid, bottom
+            ]
+        };
+    },
+
+    /*
+     * Utility function for turning indexed vertices into a "raw" coordinate array
+     * arranged as triangles.
+     */
+    toRawTriangleArray: function (indexedVertices) {
+        var result = [],
+            i,
+            j,
+            maxi,
+            maxj;
+
+        for (i = 0, maxi = indexedVertices.indices.length; i < maxi; i += 1) {
+            for (j = 0, maxj = indexedVertices.indices[i].length; j < maxj; j += 1) {
+                result = result.concat(
+                    indexedVertices.vertices[
+                        indexedVertices.indices[i][j]
+                    ]
+                );
+            }
+        }
+
+        return result;
+    },
+
+    /*
+     * Utility function for turning indexed vertices into a "raw" coordinate array
+     * arranged as line segments.
+     */
+    toRawLineArray: function (indexedVertices) {
+        var result = [],
+            i,
+            j,
+            maxi,
+            maxj;
+
+        for (i = 0, maxi = indexedVertices.indices.length; i < maxi; i += 1) {
+            for (j = 0, maxj = indexedVertices.indices[i].length; j < maxj; j += 1) {
+                result = result.concat(
+                    indexedVertices.vertices[
+                        indexedVertices.indices[i][j]
+                    ],
+
+                    indexedVertices.vertices[
+                        indexedVertices.indices[i][(j + 1) % maxj]
+                    ]
+                );
+            }
+        }
+
+        return result;
+    }
+
+};
